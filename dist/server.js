12b5455390e8bf4fadbbebe95bc0a6d6b3b5e401
Lane Faison <lfaison@tangogroup.com>
2019-01-10 22:09:15 -0700

    Added routes

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-app.get('/', (req, res) => res.send('Hello World!!!!'));
+// Can add anything after the / to make custom get routes
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/colorado', (req, res) => res.send('Hello Colorado!'));
+app.get('/lane', (req, res) => res.send('Hello Lane!'));
 app.listen(3000, () => console.log('Example app listening on port 3000'));
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!!!!'));
+// Can add anything after the / to make custom get routes
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/colorado', (req, res) => res.send('Hello Colorado!'));
+app.get('/lane', (req, res) => res.send('Hello Lane!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000'));
